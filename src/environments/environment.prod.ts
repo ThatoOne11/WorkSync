a8d9069fc9adde134934cb089e4632aaec61f431
Thato <thato@oneeleven.tech>
2025-10-12 12:09:49 +0200

    chore: update environment variabales

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
-  supabaseUrl: 'YOUR_SUPABASE_URL',
-  supabaseKey: 'YOUR_SUPABASE_ANON_KEY'
+  supabaseUrl: 'https://pzenvwabjqakdcclraav.supabase.co',
+  supabaseKey:
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB6ZW52d2FianFha2RjY2xyYWF2Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc2MDE2OTIxNSwiZXhwIjoyMDc1NzQ1MjE1fQ.T9h9do_LfQxnHj7sJVgZhrSVOR_v4JgjhVcfjdrsZzE',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
-  supabaseUrl: 'http://127.0.0.1:54321',
-  supabaseKey: 'sb_publishable_ACJWlzQHlZjBrEguHvfOxg_3BJgxAaH',
+  supabaseUrl: 'https://pzenvwabjqakdcclraav.supabase.co',
+  supabaseKey:
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB6ZW52d2FianFha2RjY2xyYWF2Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc2MDE2OTIxNSwiZXhwIjoyMDc1NzQ1MjE1fQ.T9h9do_LfQxnHj7sJVgZhrSVOR_v4JgjhVcfjdrsZzE',
 };
